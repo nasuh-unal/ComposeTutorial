8fd018955118b82273bffc2f8c6b713cb8c53a61
Nasuh Unal <nasuhunal1@gmail.com>
2024-03-01 21:53:23 +0300

    Repository-and-DependencyInjection

--- MarsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -1,12 +1,20 @@
 package com.example.marsphotos.ui.screens
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.marsphotos.MarsPhotosApplication
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.marsphotos.data.MarsPhotosRepository
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
+import com.example.marsphotos.model.MarsPhoto
 import com.example.marsphotos.network.MarsApi
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
@@ -17,7 +25,7 @@ sealed interface MarsUiState {
     object Loading : MarsUiState
 }
 
-class MarsViewModel : ViewModel() {
+class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
     var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
@@ -29,14 +37,33 @@ class MarsViewModel : ViewModel() {
     fun getMarsPhotos() {
         viewModelScope.launch {
             //delay(5000)
+            /*
+            ViewModel'in veriler için doğrudan ağ isteği yapması yerine, repository verileri sağlar.
+            ViewModel artık doğrudan MarsApi koduna başvurmaz.
+            Bu yaklaşım, verileri alan kodun ViewModel'den bağımsız olmasına yardımcı olur.
+            Depo getMarsPhotos() adında bir fonksiyona sahip olduğu sürece, gevşek bağlı olmak
+            ViewModel veya depoda diğerini olumsuz etkilemeden değişiklik yapılmasına olanak tanır.
+             */
             marsUiState = MarsUiState.Loading
             marsUiState = try {
-                val listResult = MarsApi.retrofitService.getPhotos()
+                //val marsPhotosRepository = NetworkMarsPhotosRepository()
+                val listResult = marsPhotosRepository.getMarsPhotos()
+                //val listResult = MarsApi.retrofitService.getPhotos()
                 MarsUiState.Success("Success: ${listResult.size} Mars photos retrieved")
             } catch (e: IOException) {
                 MarsUiState.Error
             } catch (e: HttpException) {
                 MarsUiState.Error
+            }
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as MarsPhotosApplication)
+                val marsPhotosRepository = application.container.marsPhotosRepository
+                MarsViewModel(marsPhotosRepository = marsPhotosRepository)
             }
         }
     }
